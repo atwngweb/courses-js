a848663ee0268c1e8fa40b8e932798cf2cc75e85
anhtuan12 <tuan.panda2004@gmail.com>
2026-05-27 02:40:55 +0700

    i++ và ++i

--- Toán-tử-và-hàm/main.js
+++ Toán-tử-và-hàm/main.js
@@ -19,3 +19,39 @@ function sum(a, b) {
 }
 
 console.log(sum(3, 7));
+
+// i++ và ++i
+
+let i = 5;
+
+console.log(i++); // 5 => Trả về giá trị của biến
+
+console.log(i); // 6  => RỒI MỚI TĂNG giá trị của biến
+
+let j = 1;
+
+console.log(++j); // 2 => tăng giá trị của biến
+
+console.log(j); // 2 => Trả về giá trị
+
+// exaple
+
+let a = 1;
+console.log("Check");
+
+console.log(++a); // 2
+console.log(a++); // 2
+console.log(a--); // 3
+console.log(--a); // 1
+console.log(++a); // 2
+
+let res = ++a + a++ - a-- + --a + ++a;
+
+console.log("gia tri cua res = ", res); // 4
+
+// ++a = 2
+// a++ = 2
+// => a = 3
+// a-- = 3
+// --a = 1
+// ++a = 2
