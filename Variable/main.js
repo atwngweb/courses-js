cdda0cbfcab8d7fbccfc643c4450f9ab9374459d
anhtuan12 <tuan.panda2004@gmail.com>
2026-05-27 01:58:11 +0700

    Variable

---
+++ Variable/main.js
@@ -0,0 +1,22 @@
+// Biến khi chưa đc gán giá trị => mặc định undefined
+
+const nameStore = "Levents";
+
+let chuVi = 5;
+
+var dienTich = 20;
+
+console.log(nameStore);
+console.log(chuVi);
+console.log(dienTich);
+
+// Cap nhat gia tri cua variable
+
+// nameStore = "Degrey"; // Báo lỗi Assignment to constant variable.
+// Do const khi khai báo biến là hằng kh thể thay đổi
+
+chuVi = 10;
+
+dienTich = 20;
+
+console.log(chuVi, dienTich);
