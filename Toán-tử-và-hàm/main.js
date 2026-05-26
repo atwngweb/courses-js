7657a6f75567745bb016cfd305398a72769a9630
anhtuan12 <tuan.panda2004@gmail.com>
2026-05-27 02:18:27 +0700

    hàm trong js

---
+++ Toán-tử-và-hàm/main.js
@@ -0,0 +1,21 @@
+// Định nghĩa hàm
+
+// Syntax: function ten_ham(tham_so) {
+// code
+// }
+
+// Gọi hàm
+// ten_ham(truyen_doi_so);
+
+// Tham số hàm có hoặc không
+
+// Giá trị truyền vào khi gọi hàm là đối số
+
+// Còn khi định nghĩa hàm là tham số
+
+// Return value
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(3, 7));
