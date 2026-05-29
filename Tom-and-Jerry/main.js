a674ca15dd0f9b397cfeec1ce7e7bc9c51575f94
anhtuan12 <tuan.panda2004@gmail.com>
2026-05-29 17:57:36 +0700

    Hàm-tạo-Function-Constuctor

--- Tom-and-Jerry/main.js
+++ Tom-and-Jerry/main.js
@@ -1,51 +1,80 @@
-// Tom and Jerry
-
-/*
-    Health/hp: Mau
-    Attack power/atk: Sat thuong
-    defense: phong thu
-*/
-
-const tom = {
-  name: "Tom",
-  hp: 1000,
-  atk: 70,
-  defense: 50,
-  //   Phuong thuc tan cong
-  attack(aim) {
+// Hàm tạo(Bản thiết kế đối tượng) - Function/Object Constructor
+// Không tạo hàm tạo bằng arrow function
+function Charater(name, hp, atk, defense) {
+  this.name = name;
+  this.hp = hp;
+  this.atk = atk;
+  this.defense = defense;
+  this.attack = (aim) => {
     const damage = this.atk - aim.defense < 0 ? 0 : this.atk - aim.defense;
     aim.hp -= damage;
     console.log(
       `${this.name} danh ${aim.name} mat ${damage} mau, ${aim.name} con ${aim.hp} mau.`,
     );
-  },
-  //   Check health
-  isAlive() {
-    if (this.hp > 0) {
-      return true;
-    } else return false;
-  },
-};
+  };
 
-const jerry = {
-  name: "Jerry",
-  hp: 400,
-  atk: 40,
-  defense: 10,
-  attack(aim) {
-    const damage = this.atk - aim.defense < 0 ? 0 : this.atk - aim.defense;
-    aim.hp -= damage;
-    console.log(
-      `${this.name} danh ${aim.name} mat ${damage} mau, ${aim.name} con ${aim.hp} mau.`,
-    );
-  },
-  //   Check health
-  isAlive() {
+  this.isAlive = () => {
     if (this.hp > 0) {
       return true;
     } else return false;
-  },
-};
+  };
+}
+
+// Tạo ra Tom
+const tom = new Charater("Tom", 1000, 70, 20);
+// Tạo ra Jerry
+const jerry = new Charater("Jerry", 400, 40, 10);
+
+// console.log(tom, jerry);
+
+// // Tom and Jerry
+
+// /*
+//     Health/hp: Mau
+//     Attack power/atk: Sat thuong
+//     defense: phong thu
+// */
+
+// const tom = {
+//   name: "Tom",
+//   hp: 1000,
+//   atk: 70,
+//   defense: 50,
+//   //   Phuong thuc tan cong
+//   attack(aim) {
+//     const damage = this.atk - aim.defense < 0 ? 0 : this.atk - aim.defense;
+//     aim.hp -= damage;
+//     console.log(
+//       `${this.name} danh ${aim.name} mat ${damage} mau, ${aim.name} con ${aim.hp} mau.`,
+//     );
+//   },
+//   //   Check health
+//   isAlive() {
+//     if (this.hp > 0) {
+//       return true;
+//     } else return false;
+//   },
+// };
+
+// const jerry = {
+//   name: "Jerry",
+//   hp: 400,
+//   atk: 40,
+//   defense: 10,
+//   attack(aim) {
+//     const damage = this.atk - aim.defense < 0 ? 0 : this.atk - aim.defense;
+//     aim.hp -= damage;
+//     console.log(
+//       `${this.name} danh ${aim.name} mat ${damage} mau, ${aim.name} con ${aim.hp} mau.`,
+//     );
+//   },
+//   //   Check health
+//   isAlive() {
+//     if (this.hp > 0) {
+//       return true;
+//     } else return false;
+//   },
+// };
 
 let round = 1;
 while (tom.isAlive() && jerry.isAlive()) {
