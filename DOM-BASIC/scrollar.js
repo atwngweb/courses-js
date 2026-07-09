0423c6f4424c964210f9c1a1427be7afb24df536
anhtuan12 <tuan.panda2004@gmail.com>
2026-07-09 16:26:11 +0700

    scroll

---
+++ DOM-BASIC/scrollar.js
@@ -0,0 +1,15 @@
+const html = document.documentElement;
+
+console.log(html.scrollTop); // Lấy ra vị trí của thanh cuộn dọc
+
+console.log(html.scrollLeft); // Lấy ra vị trí của thanh cuộn ngang
+
+console.log(html.scrollHeight); // Lấy ra chiều cao phần cuộn
+
+console.log(html.scrollWidth); // Lấy ra chiều ngang của phần cuộn
+
+// html.scrollTo(x, y); di chuyển thanh cuộn tới vị trí mong muốn
+
+// html.scrollBy(x, y); di chuyển thêm từ vị trí hiện tại của thanh cuộn
+
+// element.scrollIntoView(); Di chuyển tới phần tử bạn muốn xem
